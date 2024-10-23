b275c8432b2b509182f90e313a8a9d753376e5ee
d3adl0rd <id3adl0rd@gmail.com>
2024-10-24 00:47:59 +0300

    Level prototype

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,11 +1,16 @@
 use bevy::prelude::*;
-
-use super::window::*;
+use super::{level::level, player::player, window::window};
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(window::WindowSettingsPlugin);
+        app.add_plugins(
+            (
+                        level::LevelPlugin,
+                        player::PlayerPlugin,
+                        window::WindowSettingsPlugin
+            )
+        );
     }
 }
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,2 +1,4 @@
 pub mod game;
 pub mod window;
+pub mod level;
+pub mod player;
